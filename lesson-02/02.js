02426994ddbb43d986a888c5eaf1235dfaac5c07
mr.Zhuravsky <sashazhuravsky9698@gmail.com>
2025-11-26 21:34:07 +0100

    дз js 02.02.01

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -13,11 +13,11 @@ let dice2 = Math.floor(Math.random() * 6) + 1
 let isWinningDouble = (dice1 === dice2) && (dice1 > 3)
 
 
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
-} else {
-  console.log('Не выигрышный дубль.')
-}
+// console.log('Первый бросок: ' + dice1)
+// console.log('Второй бросок: ' + dice2)
+// if (isWinningDouble) {
+//   console.log('Выигрышный дубль!')
+// } else {
+//   console.log('Не выигрышный дубль.')
+// }
 
