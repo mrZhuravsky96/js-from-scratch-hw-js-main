1a8cdd51613beddcd5cf120844c2c505c4c3d388
mr.Zhuravsky <sashazhuravsky9698@gmail.com>
2025-11-27 10:05:50 +0100

    дз js 02.03

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,17 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
-let grade
-// your code
+const score = 93
+let grade 
+if (score < 50){
+    grade = 'F'
+} else if (score < 70){
+    grade = 'D'
+ }else if (score < 80){
+    grade = 'C' }
+  else if (score < 90){
+    grade = 'B'
+} else{ 
+    grade = 'A'
+  }
+console.log(grade)
