21d9d2c3f163e4dbddf2fdcdbda45047a44395d3
mr.Zhuravsky <sashazhuravsky9698@gmail.com>
2025-12-16 11:33:08 +0100

    дз js 05.01

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,17 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
-}
+  addResource(resource, amount) {
+    if (this.resources.hasOwnProperty(resource)) {
+      this.resources[resource] += amount;
+    } else {
+      console.log("Invalid resource");
+    }
+  }
+};
+
+// Примеры использования:
+game.addResource('gold', 50);
+console.log(game.resources.gold); // 300
+
+game.addResource('stone', 30); // "Invalid resource"
